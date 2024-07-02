3c278eb00828d100ea82d4c3812b3381cd926478
Hey-Mynk <mail.contactmayank@gmail.com>
2024-07-02 18:29:11 +0530

    commit new

--- force-app/main/default/lwc/emailMessage/emailMessage.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api, wire } from 'lwc';
-import getEmailMessages from '@salesforce/apex/LeadInteractionHandler.getLatestEmailMessageForLead';
-
-export default class EmailMessageViewer extends LightningElement {
-    @api recordId;
-    
-    @wire(getEmailMessages, { leadId: '$recordId' }) emailMessages;
-}
